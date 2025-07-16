44de7565897b1488779a6e5a19de425baf6c9d05
vignesh G <vigneshg@vigneshs-MacBook-Air.local>
2025-07-16 15:22:59 +0530

    Fix: enable rack-cors in production

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,7 +1,7 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001'  
+    origins 'http://localhost:3001', 'https://hfw-bevd-vigneshgnanasekaran27s-projects.vercel.app'
 
     resource '*',
       headers: :any,
