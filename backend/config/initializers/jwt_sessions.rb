3495939b9e8639cd5d4e87a5c82c68dfdf6cdd0d
vignesh G <vigneshgnanasekaran27@gmail.com>
2025-07-17 12:09:54 +0530

    production implemented radis

--- backend/config/initializers/jwt_sessions.rb
+++ backend/config/initializers/jwt_sessions.rb
@@ -1,2 +1,4 @@
 # config/initializers/jwt_sessions.rb
+require "redis"
 JWTSessions.encryption_key = Rails.application.credentials.secret_key_base
+JWTSessions.token_store = :redis, { redis_url: ENV["REDIS_URL"] || "redis://localhost:6379/0" }
